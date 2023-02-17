bfd6ae571075a5432ec45062cd5d4b9112a5cf14
Sarjan Patel <77773715+sp190603@users.noreply.github.com>
2023-02-17 19:56:53 +0530

    initial template design
    initial template design

---
+++ header/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="navbar navbar-expand-lg" style="background-color: #252525;">
+<img src="header/mainlogo.png" height="120" style="margin-left: 15px; margin-top:5px;">
+<div class="collapse navbar-collapse col-10" id="navbarNav">
+  <!-- <span style="color: #14FFEC; font-family: fantasy; font-size:40px; margin-left: 15px; margin-top:5px;" id="fnt1">AtreyaShield.in</span> -->
+  <ul class="navbar-nav ms-auto">
+
+    <li class="nav-item btn">
+      <button class="btn" style="background-color: #CCFF00; margin-right: 15px;"><a class="nav-link" href="signup.php"
+          style="color: #000000">Login</a></button>
+    </li>
+  </ul>
+</div>
+</div>
+<hr style="color:#CCFF00; margin-top:-5px;">
+</body>
+</html>
